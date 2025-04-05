9d5a881b995e8af7cb117837522c450cb3cace59
Vivek18 <viveksaha096@gmail.com>
2025-04-05 11:38:11 +0530

    final changes done

--- LINKEDLIST/MEDLVL SLL/medlvl.cpp
+++ LINKEDLIST/MEDLVL SLL/medlvl.cpp
@@ -219,7 +219,28 @@ bool isPalindrome(Node* head) {
 
 /*
 Optimal Approach 
+In optimal Approach we donot use any extra space.
+Observation 
+If i divide the links in two halfs and in the second half i just reverse the links and from two halfs i check every element of one half matches with the other half it it is then return true otherwise return false.
+First I have to divide the two halfs and settle down their heads
+so we use the technique of tortise and hary method
+For even 
+1->2->3->-3->2->1->X
+Here when slow is in the 3 place the fast will be in the 5th place so the condition apply to run till fast->next->next != null
+For odd
+1->2->3->2->1->X
+Here slow reaches the middle at 3rd place while fast reaches the 5th place and here the condition apply to run till fast->next != null
 
+Now we get our middle 
+it's time to reverse the second half 
+
+reverse(slow->next)
+
+then take a pointer in first half head and another for the second half head and check wheather they are equal or not 
+
+
+Time Complexity : O(n/2) (for finding middles) + O(n/2) (for reverse) + O(n/2) (for traversing) nearly equal to O(n)
+Space Complexity : O(1)
 */
 Node* reverse(Node* head) {
     // Using recursive method
@@ -253,6 +274,140 @@ bool isPalindromeOptimal(Node* head){
         reverse(newHead);
         return true;
     
+}
+
+/*
+Question 6
+Brute Force Approach 
+Take an extra space and there first store odd ones and then even ones .
+Time Complexity : O(2*n)
+Space Complexity : O(n)
+*/
+void OddEven(Node* head){
+    if(head == nullptr) cout << "0";
+        vector<int> arr;
+        Node* temp = head;
+        while(temp != nullptr && temp->next != nullptr){
+            arr.push_back(temp->data);
+            temp = temp->next->next;
+        }
+        if(temp != nullptr) arr.push_back(temp->data);
+        temp = head->next;
+        while(temp != nullptr && temp->next != nullptr){
+            arr.push_back(temp->data);
+            temp = temp->next->next;
+        }
+        if(temp != nullptr) arr.push_back(temp->data);
+        temp = head;
+
+        for(int i = 0; i < arr.size(); i++){
+            temp->data = arr[i];
+            temp = temp->next;
+        }
+    while(head != nullptr){
+        cout << head->data << "-->";
+        head = head->next;
+    }
+}
+
+
+/*
+Optimal Approach
+We dont use any extra space here 
+Play with the links 
+1->2->3->4->5->X
+
+Take two pointer first one odd at first place and second one at the second place as even and move them with two places.
+1->3->5  2->4->X 
+But we have to link 5->2 to link we have link a node with odd->next because odd is at 5 so at begining store 2 at evenHead one more pointer and then
+link odd->next = evenHead
+
+Time Complexity : O(N)
+Space Complexity : O(1)
+*/
+
+void OddEvenOptimal(Node* head){
+    if(head  == nullptr || head->next == nullptr) cout << "O";
+    Node* odd = head , *evenHead = odd->next , *even = odd->next;
+    
+    while(even != nullptr && even->next != nullptr){
+        odd->next = odd->next->next;
+        even->next = even->next->next;
+
+        odd = odd->next;
+        even = even->next;
+    }
+    odd->next = evenHead;
+
+
+    while(head != nullptr){
+        cout << head->data << "-->";
+        head = head->next;
+    }
+}
+
+
+/*
+Question 6
+We have to sort the list
+We use extra space and replacement approach
+
+*/
+void sortList(Node* head){
+Node* temp = head;
+if(head == nullptr || head->next == nullptr) cout << head->data;
+vector<int> arr; // extra space
+while(temp != nullptr){
+    arr.push_back(temp->data);
+    temp = temp->next;
+}
+sort(arr.begin(),arr.end());
+temp = head;
+// replacement
+for(int i = 0; i < arr.size(); i++){
+    temp->data = arr[i];
+    temp = temp->next;
+}
+
+// Output
+while(head != nullptr){
+    cout << head->data <<"-->";
+    head = head->next;
+}
+
+}
+
+
+/*
+Question 7
+By using DummyNode Approach
+*/
+Node* addList(Node* l1, Node* l2){
+    Node* dummyNode = new Node(-1);
+    Node* curr = dummyNode;
+    Node* t1 = l1;
+    Node* t2 = l2;
+    int carry = 0;
+        while(t1 != nullptr || t2 != nullptr){
+            int sum = carry;
+            if(t1) sum = sum + t1->data;
+            if(t2) sum = sum + t2->data;
+            Node* newNode = new Node(sum % 10);
+            carry = sum/10;
+            curr->next = newNode;
+            curr = curr->next;
+            if(t1) t1 = t1->next;
+            if(t2) t2 = t2->next;
+
+            if(carry){
+                Node* newNode = new Node(carry);
+                curr->next = newNode;
+            }
+        }
+        while(dummyNode -> next != nullptr){
+            cout << dummyNode->next->data <<"-->";
+            dummyNode = dummyNode->next;
+        }
 }
 int main(){
 
@@ -349,7 +504,7 @@ Input: head = [1,2,2,1]
 Output: true
 Input: head = [1,2]
 Output: false
-*/
+
 vector<int> arr = {1,2,2,1};
 Node* head = new Node(arr[0]);
 Node* newnode = head;
@@ -365,5 +520,81 @@ cout << ans;
 bool opans = isPalindromeOptimal(head);
 
 cout << opans;
+
+*/
+/*
+Question 5 
+Odd Even Linkedlist
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+Input: head = [1,2,3,4,5]
+Output: [1,3,5,2,4]
+
+
+// Brute Force Approach 
+vector<int> arr = {1,2,3,4,5};
+Node* head = new Node(arr[0]);
+Node* newnode = head;
+for(int i = 1; i < arr.size(); i++){
+       newnode->next = new Node(arr[i]);
+       newnode = newnode->next;
+}
+
+//OddEven(head);
+cout << endl;
+
+// Optimal Approach 
+OddEvenOptimal(head);
+*/
+
+
+/*Question 6
+Given the head of a linked list, return the list after sorting it in ascending order.
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+*/
+vector<int> arr = {4,2,1,3};
+Node* head = new Node(arr[0]);
+Node* newnode = head;
+for(int i = 1; i < arr.size(); i++){
+       newnode->next = new Node(arr[i]);
+       newnode = newnode->next;
+}
+
+sortList(head);
+
+
+/*
+Question 7
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+*/
+
+vector<int> l1= {2,4,5};
+vector<int> l2 = {5,6,4};
+Node* head1 = new Node(l1[0]);
+Node* node1 = head1;
+for(int i = 1; i < l1.size(); i++){
+    node1->next = new Node(l1[i]);
+    node1 = node1->next;
+}
+Node* head2 = new Node(l2[0]);
+Node* node2 = head2;
+for(int i = 1; i < l2.size(); i++){
+       node2->next = new Node(l2[i]);
+       node2 = node2->next;
+}
+addList(head1,head2);
     return 0;
 }
---
+++ LINKEDLIST/MEDLVL SLL/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+
+// Output
+while(head != nullptr){
+    cout << head->data <<"-->";
+    head = head->next;
+}
+cout << "X";
+}
