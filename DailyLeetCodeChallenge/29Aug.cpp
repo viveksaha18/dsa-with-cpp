1e79f8dc03f6c45c626ee483e724c47e255327a1
Vivek18 <viveksaha096@gmail.com>
2025-08-29 23:56:10 +0530

    29AUg_2025

---
+++ DailyLeetCodeChallenge/29Aug.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        return 1LL * ((n+1)/2) * (m/2) + 1LL * (n/2) * ((m+1)/2);
+    }
+};
+int main() {
+    
+    return 0;
+}
