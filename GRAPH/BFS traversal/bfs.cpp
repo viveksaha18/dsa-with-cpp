264138992315b04bbaa4b5204f616164e11a27cf
Vivek18 <viveksaha096@gmail.com>
2025-09-03 10:31:38 +0530

    changes done

--- GRAPH/BFS traversal/bfs.cpp
+++ GRAPH/BFS traversal/bfs.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Problem of GFG
 /*
 Question : 
 Given a connected undirected graph containing V vertices, represented by a 2-d adjacency list adj[][], where each adj[i] represents the list of vertices connected to vertex i. Perform a Breadth First Search (BFS) traversal starting from vertex 0, visiting vertices from left to right according to the given adjacency list, and return a list containing the BFS traversal of the graph.
