54c93168a48a0b44e645262a513b086006b8fdbc
Vivek18 <viveksaha096@gmail.com>
2025-10-15 22:41:46 +0530

    Optimal Approached for the Problem Reverse Vowel of a String

--- STRING/Easy Problems/reverseVowelsofString.cpp
+++ STRING/Easy Problems/reverseVowelsofString.cpp
@@ -25,9 +25,37 @@ public:
         return s;
     }
 };
+
+// Optimal Solution 
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+class Solution1 {
+public:
+bool isVowel(char c) {
+      if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' 
+        || c=='A' || c=='E' || c=='I' || c=='O' || c=='U') return true;
+        return false;
+}
+    string reverseVowels(string s) {
+        int n = s.size();
+        int i = 0; 
+        int j = n - 1;
+        while(i < j) {
+            if(!isVowel(s[i])) i++;
+            else if(!isVowel(s[j])) j--;
+            else {
+                swap(s[i], s[j]);
+                i++, j--;
+            }
+        }
+        return s;
+    }
+};
 int main() {
     string s = "leetcode";
     Solution s1;
     cout << s1.reverseVowels(s) << endl;
+    Solution1 s2;
+    cout << s2.reverseVowels(s) << endl;
     return 0;
 }
