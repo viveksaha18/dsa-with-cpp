32ef05884bef52e363cef38f320913e03e055a55
Vivek18 <viveksaha096@gmail.com>
2026-03-09 23:51:09 +0530

    Basics of Inheritance Single Level Inheritance

--- OOPs/Inheritance/inheritance.cpp
+++ OOPs/Inheritance/inheritance.cpp
@@ -1,5 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
+// Inheritance -- When properties and member functions of base class are passed on to the derived class.
+// Helps in code reusability.
+class Person{
+    public:
+    string name;
+    int age;
+    // Person() {
+    //     cout << "Parent Class Constructor" << endl;
+    // }
+    // void parentMethod() {
+    //     cout << "This is a method of parent class" << endl;
+    // }
+
+    // Parameterized constructor
+    Person(string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+};
+class Student : public Person{
+    public:
+    int rollno;
+    // Student() {
+    //     cout << "Derived Class Constructor"<< endl;
+    // }
+    Student(string name, int age, int rollno) : Person(name, age) {
+        this->rollno = rollno;
+    }
+    void getInfo() {
+        cout << name << " " << age << " " << rollno << endl;
+    }
+};
 int main(){
+    Student s("Vivek", 21, 101);
+    // s.name = "Vivek";
+    // s.age = 21;
+    // s.rollno = 101;
+    s.getInfo();
+    // s.parentMethod();
+    
     return 0;
 }
